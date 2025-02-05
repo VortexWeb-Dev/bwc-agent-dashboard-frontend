deffe5a0af6928effd49d9970c56d1da38289808
Rohan <rohan.2021b0131077@abes.ac.in>
2025-02-05 16:05:49 +0530

    removed copyright footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,6 @@ const Footer = () => {
     <div className='  text-gray-600 text-xl p-4 bg-blue-100'>
         
       <div className='float-left'>Blue World City</div>
-      <div className='float-right'>2024, made with ❤️ by BGC</div>
     </div>
   )
 }
